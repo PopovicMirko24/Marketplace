7e5b35b9d008fb70729ab16971be9b4722e447dd
Mirko Popovic <popovicmirko78@gmail.com>
2024-05-27 22:46:50 +0200

    Migration for product table, UserController and ProductController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function login(Request $request){
+        $incomingFields = $request->validate([
+            'loginEmail' => 'required',
+            'loginPassword' => 'required'
+        ]);
+
+        if(auth()->attempt(['email' => $incomingFields['loginEmail'], 'password' => $incomingFields['loginPassword']])){
+            $request->session()->regenerate();
+        }else{
+            return redirect('login');
+        }
+
+        return redirect()->back();
+    }
+
+    public function register(Request $request){
+        $incomingFields = $request->validate([
+            'name' => 'required',
+            'lastname' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+            'confirmPassword' => 'required|same:password',
+        ]);
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+
+        $user = User::create($incomingFields);
+        auth()->login($user);
+        return redirect('/');
+    }
+
+    public function logout(){
+        auth()->logout();
+        return redirect('/');
+    }
+}
